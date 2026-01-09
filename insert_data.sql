cfd77bb9cc051646a39c6447f8dbf4a15b27f587
khadetanuja2003-afk <khadetanuja2003@gmail.com>
2026-01-09 13:30:34 +0530

    Add files via upload

---
+++ insert_data.sql
@@ -0,0 +1,25 @@
+INSERT INTO customers (customer_name, email, city, credit_limit)
+VALUES
+('Amit Sharma', 'amit@gmail.com', 'Pune', 60000),
+('Neha Patil', 'neha@gmail.com', 'Mumbai', 40000),
+('Rahul Deshmukh', 'rahul@gmail.com', 'Pune', 80000),
+('Sneha Kulkarni', 'sneha@gmail.com', 'Nashik', 55000),
+('Rohit Patil', 'rohit@gmail.com', 'Kolhapur', 30000),
+('Priya Joshi', 'priya@gmail.com', 'Pune', 70000),
+('Ankit Verma', 'ankit@gmail.com', 'Nagpur', 45000),
+('Kiran Jadhav', 'kiran@gmail.com', 'Satara', 52000),
+('Pooja Mehta', 'pooja@gmail.com', 'Mumbai', 90000),
+('Saurabh Desai', 'saurabh@gmail.com', 'Pune', 35000);
+
+INSERT INTO orders (customer_id, order_date, amount)
+VALUES
+(1, '2024-01-10', 12000),
+(2, '2024-01-12', 8000),
+(3, '2024-01-15', 15000),
+(4, '2024-01-18', 22000),
+(5, '2024-01-20', 5000),
+(6, '2024-01-22', 18000),
+(7, '2024-01-25', 9000),
+(8, '2024-01-27', 14000),
+(9, '2024-01-29', 30000),
+(10, '2024-01-30', 11000);
